b3a38c5a45f650e3e4706bb8069ed7648160ad26
Tushar <tusharkaler450@gmail.com>
2023-12-12 01:06:09 +0530

    3d effect

--- script.js
+++ script.js
@@ -171,3 +171,34 @@ btnB.addEventListener('click', () => {
         body.classList.toggle('redMode');
     }
 });
+
+
+
+// <==============on cursor movement==============>
+
+let constrain = 80;
+let mouseOverContainer = document.getElementById("hi2");
+let ex1Layer = document.getElementById("myimg");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(1200px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
