81a77a52231183c43bcdb5bcdf4a7260cc53af55
Tushar <tusharkaler450@gmail.com>
2023-12-18 11:09:39 +0530

    cursor

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//<-----------------cursor----------------->
+
+const cursor = document.getElementById("cursor-custom"),
+  radius = cursor.offsetHeight / 2;
+
+document.addEventListener("mousemove", (e) => {
+  let top = e.clientY - radius,
+    left = e.clientX - radius;
+
+  cursor.style = `top: ${top}px; left: ${left}px`;
+});
+
 // <--------------Preloader--------------->
 let preloader = document.getElementById('preloader');
 window.addEventListener('load', () => {
