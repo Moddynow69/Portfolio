58ed31134acf680946dc8c2fa5891c89ce92155e
Tushar <tusharkaler450@gmail.com>
2023-12-25 12:29:00 +0530

    cursor fix

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 //<-----------------cursor----------------->
 
 const cursor = document.getElementById("cursor-custom"),
-  radius = cursor.offsetHeight / 2;
+    radius = cursor.offsetHeight / 2;
 
 document.addEventListener("mousemove", (e) => {
-  let top = e.clientY - radius,
-    left = e.clientX - radius;
-
-  cursor.style = `top: ${top}px; left: ${left}px`;
+    let top = e.clientY - radius,
+        left = e.clientX - radius;
+    cursor.style = `top: ${top}px; left: ${left}px; transition: 0.5s; transition-timing-function: ease-out;`;
 });
 
 // <--------------Preloader--------------->
